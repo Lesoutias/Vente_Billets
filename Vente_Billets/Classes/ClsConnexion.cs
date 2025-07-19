1eb56fa662c8795db706e69b5678a46e7d53ae93
itangishakabanzirafiston <itangishakafiston80@gmail.com>
2025-07-19 07:59:54 +0200

    Add project files.

---
+++ Vente_Billets/Classes/ClsConnexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vente_Billets.Classes
+{
+    class ClsConnexion
+    {
+        public static string waybd = "data source = METRE\\SQLEXPRESS; initial catalog = venteBilletBd; integrate security = True; encrypt = false;";
+    }
+}
---
+++ Vente_Billets/Classes/ClsDict.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Vente_Billets.Classes
+{
+    class ClsDict
+    {
+        static ClsDict instance = null;
+
+        public static ClsDict Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ClsDict();
+                return instance;
+            }
+        }
+
+        SqlConnection con = null;
+        
+
+        public void OpenConnection()
+        {
+            con = new SqlConnection(ClsConnexion.waybd);
+            con.Open();
+        }
+    }
+}
